8c1eb0a911c4fe3c407c513fc55f04ab57310841
Michael Spang <mspang@csclub.uwaterloo.ca>
2009-09-10 14:40:17 -0400

    Move some code

---
+++ ceo/ops.py
@@ -0,0 +1,21 @@
+import os, syslog, grp
+
+def response_message(response, status, message):
+    priority = syslog.LOG_ERR if status else syslog.LOG_INFO
+    syslog.syslog(priority, message)
+    msg = response.messages.add()
+    msg.status = status
+    msg.message = message
+    return status
+
+def get_ceo_user():
+    user = os.environ.get('CEO_USER')
+    if not user:
+        raise Exception("environment variable CEO_USER not set");
+    return user
+
+def check_group(user, group):
+    try:
+        return user in grp.getgrnam(group).gr_mem
+    except KeyError:
+        return False
